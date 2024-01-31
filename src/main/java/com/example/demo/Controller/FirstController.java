ec47b30cdb3386b8e1e53412d7780a2db1b4151f
4iTech-john <4iTech-john.com>
2024-01-31 22:01:36 +0800

    3

--- src/main/java/com/example/demo/Controller/FirstController.java
+++ src/main/java/com/example/demo/Controller/FirstController.java
@@ -1,15 +1,28 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class FirstController {
 
+    @Autowired
+    TestService testService;
+
     @GetMapping("hello")
     public String HelloWorld(){
         return "HelloWorld";
+    }
+
+    @PostMapping("getToken")
+    public String test() throws IOException {
+        return testService.test();
     }
 }
